8817958dfb1dd2041425c9806064d703fa7925d0
unknown <binhtranthe2005@gmail.com>
2025-05-26 01:39:40 +0700

    TtbLesson04

---
+++ TtbLesson04/Models/TtbBook.cs
@@ -0,0 +1,13 @@
+﻿namespace TtbLesson04.Models
+{
+    public class TtbBook
+    {
+        public string TtbId  { get; set; }
+        public string TtbTitle { get; set; } 
+        public string TtbDescription { get; set; } 
+        public string TtbImage { get; set; }          
+        public float TtbPrice { get; set; } 
+        public int TtbPage {  get; set; } 
+
+    }
+}
